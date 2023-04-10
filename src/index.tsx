89460f1c237414b7d94a0d73bc68658b2a2bd190
anhdtl <lananhdao1303@gmail.com>
2023-04-10 12:14:03 +0700

    update

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "../src/styles/index.css";
 import App from "./App";
 import "./i18n/i18n";
-
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
